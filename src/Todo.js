5c7a0da73bae53e5b62675cab473ceda12bac86f
Salonii02 <salonisawarkar13@gmail.com>
2020-12-27 01:52:23 +0530

    Created simple todo

---
+++ src/Todo.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import "./Todo.css";
+export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      task: this.props.task
+    };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+  handleRemove() {
+    this.props.removeTodo(this.props.id);
+  }
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+  handleUpdate(evt) {
+    evt.preventDefault();
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+  handleToggle() {
+    this.props.toggleTodo(this.props.id);
+  }
+  render() {
+    console.log("This is Todo");
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <div className="todo">
+          <form onSubmit={this.handleUpdate}>
+            <input
+              type="text"
+              name="task"
+              value={this.state.task}
+              onChange={this.handleChange}
+            />
+            <button className="fa fa-save"></button>
+          </form>
+        </div>
+      );
+    } else {
+      result = (
+        <div className="todo">
+          <li
+            className={this.props.completed ? "completed" : "notcompleted"}
+            onClick={this.handleToggle}
+          >
+            {this.props.task}
+          </li>
+          <button className="fa fa-edit" onClick={this.toggleForm}></button>
+          <button
+            className="fa fa-trash-o"
+            onClick={this.handleRemove}
+          ></button>
+        </div>
+      );
+    }
+    return result;
+  }
+}
